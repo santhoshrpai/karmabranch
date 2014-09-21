cb59638924f709ffd16d7c9edf3627402e083825
cdhekne <chinmaydhekne@gmail.com>
2014-09-20 22:07:38 -0700

    Main Activity

--- src/com/example/karmabranch/MainActivity.java
+++ src/com/example/karmabranch/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.karmabranch;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,53 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button button_5min = (Button) findViewById(R.id.button_5min);
+		Button button_10min = (Button) findViewById(R.id.button_10min);
+		Button button_1hr = (Button) findViewById(R.id.button_1hr);
+		Button button_login = (Button) findViewById(R.id.button_login);
+		Button button_register = (Button) findViewById(R.id.button_register);
+
+		button_5min.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				/*Intent i=new Intent(MainActivity.this,LoginActivity.class);
+				startActivity(i);*/
+			}
+		});
+		button_10min.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				/*Intent i=new Intent(MainActivity.this,LoginActivity.class);
+				startActivity(i);*/
+			}
+		});
+		button_1hr.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				/*Intent i=new Intent(MainActivity.this,LoginActivity.class);
+				startActivity(i);*/
+			}
+		});
+		button_login.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i=new Intent(MainActivity.this,LoginActivity.class);
+				startActivity(i);
+			}
+		});
+		button_register.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				/*Intent i=new Intent(MainActivity.this,LoginActivity.class);
+				startActivity(i);*/
+			}
+		});
+
 	}
 
 	@Override
